c6ee264a663ffee22a949b3712e8c6bbef8573b3
Christopher Reid <chris@onthisspot.ca>
2024-12-06 16:34:03 -0800

    part 3 complete... slow tho... how to do a similar cursor implementation faster? no iterators... just slices?

--- src/003/003.rs
+++ src/003/003.rs
@@ -5,21 +5,10 @@ const FILENAME: &str = "data";
 fn parse_input () -> String {
     read_to_string(FILENAME)
         .unwrap()
-        // .lines()
-        // .map(|s| s.split_whitespace()
-        //     .map(|s| s.parse::<i32>().unwrap())
-        //     .collect::<Vec<i32>>()
-        // ).collect::<Vec<Vec<i32>>>()
 }
 
-fn main() {
-    let data = parse_input();
-
-    let chars_with_indexes = data.char_indices();
-    let count = chars_with_indexes.count();
-    let cursor = 0;
-
-    let two = data
+fn part_1 (data: String) -> () {
+    let data = data
         .split("mul(")
         .map(String::from)
         .map(
@@ -29,7 +18,7 @@ fn main() {
                 .to_string()
         ).collect::<Vec<String>>();
 
-    let two = &two
+    let data = &data
         .iter()
         .map(
             |s| s.split(",")
@@ -41,10 +30,9 @@ fn main() {
                 Ok(_)=> true,
                 Err(__)=> false
             }
-            )
-        ).collect::<Vec<_>>();
+        )).collect::<Vec<_>>();
 
-    let mut two = &two.iter()
+    let mut data = &data.iter()
         .map(|e| e.iter()
             .map(|f| f.parse::<i32>().unwrap()
             ).collect::<Vec<_>>()
@@ -52,18 +40,97 @@ fn main() {
 
     let mut total = 0;
 
-    for pair in two  {
+    for pair in data  {
         let product = pair.iter().copied().reduce(|a,b| a*b).unwrap();
-        println!("{}", product);
         total += product;
     }
 
+    println!("part1: {total}");
+}
 
+fn main() {
 
+    let data = parse_input();
 
+    part_1(data.clone());
 
+    let mut multing = false;
+    let mut dont_do_it = false;
+    let mut cursor: usize = 0;
+    let chars_length = data.chars().count();
 
+    let mut total_2 = 0;
 
-    println!("{total}");
+    while cursor < chars_length {
+
+        let current_char = data.chars().nth(cursor).unwrap();
+
+        match current_char {
+            'm' => {
+                let next_four = data.chars().skip(cursor).take(4).collect::<String>();
+                if !dont_do_it {
+                    multing = next_four == "mul(";
+                    cursor += if multing { 4 } else { 1 };   
+                } else {
+                    cursor += 1;
+                }
+            },
+            'd' => {
+                let next_seven = data.chars().skip(cursor).take(7).collect::<String>();
+                let do_ = &next_seven[0..4] == "do()";
+                let dont_ = &next_seven == "don't()";
+                if dont_ { 
+                    dont_do_it = true; 
+                    cursor += 7;
+                    continue;
+                }
+                if do_ && dont_do_it { 
+                    dont_do_it = false; 
+                    cursor += 4;
+                    continue;
+                }
+                cursor += 1;
+            },
+            _ => {
+                if multing && !dont_do_it {
+                    let until_close = data.chars().skip(cursor).take_while(|c| *c != ')').collect::<String>();
+                    if until_close.chars().count() > 7 {
+                        cursor += 1;
+                        multing = false;
+                    } else {
+                        let pair = until_close.split(",").collect::<Vec<_>>();
+                        if pair.len() == 2 {
+                                let both_ints = pair.iter().all(
+                                    |f| match f.parse::<i32>() {
+                                        Ok(_)=> true,
+                                        Err(__)=> false
+                                    }
+                                );
+                                if both_ints {
+                                    let pair = pair.iter().map(|e| e.parse::<i32>().unwrap()).collect::<Vec<_>>();
+                                    let product = pair.iter().copied().reduce(|a,b| a*b).unwrap();
+                                    total_2 += product;
+                                    cursor += until_close.chars().count();
+                                    multing = false;
+                                } else {
+                                    cursor += 1;
+                                    multing = false;
+                                }
+                        } else {
+                            cursor += 1;
+                            multing = false;
+                        }
+                    }
+                } else {
+                    cursor += 1;
+                }
+            }
+        }
+    }
+
+    println!("part2: {}", total_2);
+
+    return;
+
 
 }
